37c3500d1c0690028351480d83263081690dbea8
Devin Conroy <d.conroy1991@gmail.com>
2020-04-16 09:31:20 -0400

    got further into my work

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-function applyGoatMode(){
-    //select body element
-    const bodyElement = document.querySelector("body");
-    // apply class of goat mode to body element
-    bodyElement.classList.add("goat-mode");
+// function applyGoatMode(){
+//     //select body element
+//     const bodyElement = document.querySelector("body");
+//     // apply class of goat mode to body element
+//     bodyElement.classList.add("goat-mode");
 
-    const listItems = document.querySelectorAll(".list-item")
-    console.log("should be all items", listItems)
-    listItems.classList.add("orange-backround");
-    for(let i = -0;)
+//     const listItems = document.querySelectorAll(".list-item")
+//     console.log("should be all items", listItems)
+//     listItems.classList.add("orange-backround");
+//     for(let i = -0;)
+// }
+// applyGoatMode();
+
+function applyChanges(){
+    const articleOne = document.querySelector(".article__header")
+    articleOne.textContent = "Welcome to the Devin Blog"
+
 }
-applyGoatMode();
+
+function secondChanges(){
+const articleAll = document.querySelectorAll(".article__header")
+for(let i = 0; i < articleAll.length; i ++)
+    articleAll[i].classList.add("important")
+}
+
+
+applyChanges()
+secondChanges()
